c06229c5120e0945a65e3e39908d4b652144c761
Andreas Heck <aheck@gmx.de>
2016-11-15 00:36:12 +0100

    Styles QSplitter so the handles are easier to spot

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setApplicationDisplayName("SSH Commander");
 
+    // style sheets
+    app.setStyleSheet(
+            "QSplitter::handle:vertical   {height: 3px; background: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #fafafa, stop: 0.4 grey, stop: 0.5 #e7e7e7, stop: 1.0 #fafafa);}"
+            "QSplitter::handle:horizontal {width:  3px; background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #fafafa, stop: 0.4 grey, stop: 0.5 #e7e7e7, stop: 1.0 #fafafa);}"
+            );
+
     // application-wide settings for QSettings
     QCoreApplication::setOrganizationName("aheck");
     QCoreApplication::setOrganizationDomain("github.com/aheck");

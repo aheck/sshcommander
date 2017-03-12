21a43e5fb47aa926fa741e0f7dbd70c2a575a4f9
Andreas Heck <aheck@gmx.de>
2017-03-12 22:57:42 +0100

    Adds sensible default size for detached AWS console window

--- src/AWSConsoleWindow.cpp
+++ src/AWSConsoleWindow.cpp
@@ -4,6 +4,7 @@ AWSConsoleWindow::AWSConsoleWindow(QWidget *parent)
 {
     this->setLayout(new QVBoxLayout());
     this->setWindowTitle(QString("AWS Console - ") + PROGRAM_NAME);
+    this->resize(1024, 768);
 }
 
 void AWSConsoleWindow::closeEvent(QCloseEvent *event)

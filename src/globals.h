6f6a766abcf6cd30f22921119673a2aaac77e659
Andreas Heck <aheck@gmx.de>
2020-10-03 23:40:42 +0200

    Changes version number to 0.5.1

--- src/globals.h
+++ src/globals.h
@@ -2,7 +2,7 @@
 #define GLOBALS_H
 
 #define SSHCOMMANDER_WEBSITE "https://sshcommander.com/"
-#define SSHCOMMANDER_VERSION "0.5.0 (Beta)"
+#define SSHCOMMANDER_VERSION "0.5.1 (Beta)"
 #define SSHCOMMANDER_LICENSE "\nCopyright © 2016-2020 Andreas Heck\n\n"\
     "This program is free software under the\n"\
     "terms of the GNU General Public License Version 2"
